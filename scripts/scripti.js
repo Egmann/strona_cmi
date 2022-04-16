f25e2dd9af601cdb9db0bef69b413f492cece67b
USERNAME <mateusz.ostrycharz@interia.pl>
2022-04-16 16:55:12 +0200

    idk honestly

---
+++ scripts/scripti.js
@@ -0,0 +1,10 @@
+const d = new Date();
+var day = d.getDay();
+var year =d.getFullYear();
+var month=d.getMonth();
+const f = new Date(2022,3,16);
+const f2 = new Date(2022,3,18);
+/*if(year+month+day!=f&&year+month+day!=f2)
+{
+    document.getElementById("kw").style.opacity = "0";
+}*/
